c607d08109ac027145441a829da725fd9b14e0f2
Jorge Saavedra <jsaavedra@montran.com>
2021-03-03 14:23:17 -0500

    Initial Commit

---
+++ swiftui_practice/swiftui_practiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_practiceApp.swift
+//  swiftui_practice
+//
+//  Created by Jorge Saavedra on 3/3/21.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_practiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
